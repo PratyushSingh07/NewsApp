be4d4e0ebc2476632dff2a9c686a72a8897b30d8
Pratyush Singh <aries.pratyush@gmail.com>
2023-03-01 17:55:19 +0530

    app:retrofit setup

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.utils
+
+class Constants {
+    companion object{
+        const val API_KEY="929449b69a9e4f6ba3940b01105edb68"
+        const val BASE_URL="https://newsapi.org"
+    }
+}
